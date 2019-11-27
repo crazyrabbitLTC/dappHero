073caecdd92ece06652c1fad29a76ae7bd79cb60
crazyrabbitLTC <dennison@dennisonbertram.com>
2019-11-27 15:21:36 -0500

    Added Web3, openzeppelin networkjs and polyfil babel

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom'
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import { useWeb3Injected } from "@openzeppelin/network/react";
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 
-class App extends Component {
- render() {
-   return (
-     <div className="samplereact">This is a React component inside of Webflow!</div>
-  )
-}
+function App() {
+  const [count, setCount] = useState(0);
+  const injected = useWeb3Injected();
+  const { accounts, networkId, networkName, providerName, lib, connected } = injected;
+
+  useEffect(async () => {
+    await injected.requestAuth();
+  },[])
+
+
+  console.log(injected);
+  return (
+    <div>
+      <p>You clicked {count} times.</p>
+      <button onClick={() => setCount(count + 1)}>Click Me</button>
+      <p>Accounts: {accounts} NetworkID: {networkId} NetworkNAme: {networkName} ProviderName: {providerName}
+      </p>
+    </div>
+  );
 }
 
 ReactDOM.render(
-React.createElement(App, {}, null),
-document.getElementById('react-target')
+  React.createElement(App, {}, null),
+  document.getElementById("react-target")
 );
