901d9c9d7e3b4a70de30fc7eaf9e3aa0ba2312ed
crazyrabbitLTC <dennison@dennisonbertram.com>
2019-11-29 22:46:35 -0500

    First commit

--- src/components/Web3EnableButton.js
+++ src/components/Web3EnableButton.js
@@ -6,19 +6,26 @@ const parent = document.getElementById("web3-enableButton");
 $(parent).empty();
 
 function Web3EnableButton(props) {
-
-  const {injected} = props;
- const {connected, accounts} = injected;
+  const { injected } = props;
+  const { connected, accounts } = injected;
 
   const web3Enable = async () => {
     await injected.requestAuth();
-  }
+  };
 
-  if(connected && accounts.length > 0){
-    return ReactDOM.createPortal(<a >Web3 Connected</a>, parent);
+  if (connected && accounts.length > 0) {
+    return ReactDOM.createPortal(<a>Web3 Connected</a>, parent);
   } else {
-    return ReactDOM.createPortal(<a onClick={() => {web3Enable()}}>enable MetaMask</a>, parent);
+    return ReactDOM.createPortal(
+      <a
+        onClick={() => {
+          web3Enable();
+        }}
+      >
+        enable MetaMask
+      </a>,
+      parent
+    );
   }
-
 }
 export default Web3EnableButton;
