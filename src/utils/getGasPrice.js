819b7e4cda2fee65f59449076e93e1f3ba0c8328
crazyrabbitLTC <dennison@dennisonbertram.com>
2019-11-30 15:10:36 -0500

    general contract deployment

--- src/utils/getGasPrice.js
+++ src/utils/getGasPrice.js
@@ -1,17 +1,16 @@
-import axios from "axios"
+import axios from "axios";
 
-const getGasPrice = async (gasPriceOption) => {
-let url = 'https://www.etherchain.org/api/gasPriceOracle';
+const getGasPrice = async gasPriceOption => {
+  let url = "https://www.etherchain.org/api/gasPriceOracle";
 
-try {
-  let {data: gasData} = await axios.get(url);
-  console.log("Oracle gas price: ", gasData);
-  return gasData[gasPriceOption] * 1e9;
-} catch (error) {
-  console.log("Error with gas Oracle")
-  return null
-}
-
-}
+  try {
+    let { data: gasData } = await axios.get(url);
+    console.log("Oracle gas price: ", gasData);
+    return gasData[gasPriceOption] * 1e9;
+  } catch (error) {
+    console.log("Error with gas Oracle");
+    return null;
+  }
+};
 
 export default getGasPrice;
