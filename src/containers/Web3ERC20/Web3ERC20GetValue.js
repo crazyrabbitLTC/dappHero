697e96ef1ac612c2af3621ef4fdd72ddc62e2da8
crazyrabbitLTC <dennison@dennisonbertram.com>
2019-11-30 17:06:02 -0500

    starting get value from form field

---
+++ src/containers/Web3ERC20/Web3ERC20GetValue.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect, Fragment } from "react";
+import deployContract from "./utils/deployContract";
+
+
+function Web3ERC20Deploy(props) {
+  const { injected } = props;
+  const { accounts, lib } = injected;
+
+  console.log("in Deploy")
+  if (injected && accounts && accounts.length > 0) {
+    return (
+      <Fragment>
+        <button
+          onClick={() => {
+            deployContract(lib, accounts);
+          }}
+        >
+          Deploy
+        </button>
+      </Fragment>
+    );
+  } else {
+    return (
+      <Fragment>
+        <button>Not ready</button>
+      </Fragment>
+    );
+  }
+}
+
+export default Web3ERC20Deploy;
